bcaad55f583bea89337305e7b9102600a4c05684
Fatima Zahra Hassani <fzhassani@SMB.SQLI.COM>
2022-03-17 09:36:23 +0100

    rendering flows commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
 
-platformBrowserDynamic().bootstrapModule(AppModule).catch(error => console.log(error));
+platformBrowserDynamic().bootstrapModule(AppModule);
